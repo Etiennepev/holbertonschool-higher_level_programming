8eecbffc2390b4ed23f40cac3f989bc52c6332c2
Etiennepev <etienneloichot@gmail.com>
2025-01-14 14:38:26 +0100

    Task 7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -2,8 +2,6 @@
 
 def islower(c):
     if (97 <= ord(c) <= 122):
-        print("{} is lower".format(c))
         return True
     else:
-        print("{} is upper".format(c))
         return False
