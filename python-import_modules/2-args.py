3bccea6b151157d3fc6eaed0c51dcf1a9baa9fea
Etiennepev <etienneloichot@gmail.com>
2025-01-15 12:07:22 +0100

    task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
     else:
         print("{:d} arguments:".format(len(argv) - 1))
         for i in range(1, len(argv)):
-            print("{:d}: {}".format(len(argv) - 1, argv[i]))
+            print("{:d}: {}".format(i, argv[i]))
