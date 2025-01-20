9954c0df6ece144c0074a18d343780d98ede2ea6
Etiennepev <etienneloichot@gmail.com>
2025-01-20 20:30:22 +0100

    task 7

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+
+    tuple_a = tuple_a[:2] + (0,) * (2 - len(tuple_a))
+    tuple_b = tuple_b[:2] + (0,) * (2 - len(tuple_b))
+
+    result = (a + b for a, b in zip(tuple_a, tuple_b))
+
+    return result
+    
+    
