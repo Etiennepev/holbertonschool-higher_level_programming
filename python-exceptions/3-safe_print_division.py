36560f5214a21fd2e828f2b7ba9945dc9716ac7d
Etiennepev <etienneloichot@gmail.com>
2025-01-22 15:50:59 +0100

    task3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,10 +4,8 @@ def safe_print_division(a, b):
     result = None
     try:
         result = a / b
-
     except ZeroDivisionError:
         result = None
-
     finally:
         print("Inside the result: {}".format(result))
     return result
