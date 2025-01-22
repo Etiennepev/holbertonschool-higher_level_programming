b821d37ef9fdc53bd4b57e47d2d2c9d92031aa8e
Etiennepev <etienneloichot@gmail.com>
2025-01-22 09:02:41 +0100

    task 1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,6 +3,6 @@
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
     for index in range(len(new_list)):
-        if new_list[index] == 2:
-            new_list[index] = 89
+        if new_list[index] == search:
+            new_list[index] = replace
     return new_list
