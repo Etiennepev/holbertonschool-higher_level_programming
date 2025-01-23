6d0ee6a17bd9cbe1001acfe6bea7b57d81f66602
Etiennepev <etienneloichot@gmail.com>
2025-01-23 13:01:04 +0100

    task 4

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python3
+"""
+Module 4-print_square
+Provides a function `print_square` to print square
+"""
+
 
 def print_square(size):
+    """
+    Prints a square of '#' characters of a given size.
+
+    Args:
+        size: The size of the square, must be an integer.
+
+    Returns:
+        None
+
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is less than 0.
+    """
 
     if type(size) is not int:
         raise TypeError("size must be an integer")
