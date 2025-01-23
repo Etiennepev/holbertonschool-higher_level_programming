dafef215952b6c66959dbb6f5ee387786e22126f
Etiennepev <etienneloichot@gmail.com>
2025-01-23 15:09:30 +0100

    task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,32 +1,64 @@
 #!/usr/bin/python3
 """
-Module 0-add_integer
-Provides a function `add_integer` to add two integers or floats.
+This module provides a function `add_integer` for adding two integers.
 """
 
 
 def add_integer(a, b=98):
     """
-    Adds two integers or floats (casting them to integers first).
+    Adds two integers or floats (casts them to integers if needed).
 
     Args:
-        a: The first number, must be an integer or float.
-        b: The second number, must be an integer or float (default is 98).
+        a (int, float): The first number.
+        b (int, float): The second number, default is 98.
 
     Returns:
-        The sum of a and b as an integer.
+        int: The addition of `a` and `b`.
 
     Raises:
-        TypeError: If a or b are not integers or floats.
+        TypeError: If `a` or `b` is not an integer or float.
+
+    Examples:
+        >>> add_integer(1, 2)
+        3
+        >>> add_integer(100, -2)
+        98
+        >>> add_integer(2)
+        100
+        >>> add_integer(100.3, -2)
+        98
+        >>> add_integer(4, "School")
+        Traceback (most recent call last):
+        ...
+        TypeError: b must be an integer
+        >>> add_integer(None, 2)
+        Traceback (most recent call last):
+        ...
+        TypeError: a must be an integer
+        >>> add_integer(2, None)
+        Traceback (most recent call last):
+        ...
+        TypeError: b must be an integer
+
+        >>> add_integer(2, float('-inf'))
+Traceback (most recent call last):
+    ...
+TypeError: b must be a valid number
+
+>>> add_integer(float('nan'), 2)
+Traceback (most recent call last):
+    ...
+TypeError: a must be a valid number
     """
 
-    if not isinstance(a, (int, float)):
+    if not isinstance(a, (int, float)) or a is None:
         raise TypeError("a must be an integer")
+    if isinstance(a, float) and (a != a or a in (float('inf'), float('-inf'))):
+        raise TypeError("a must be a valid number")
 
-    if not isinstance(b, (int, float)):
+    if not isinstance(b, (int, float)) or b is None:
         raise TypeError("b must be an integer")
+    if isinstance(b, float) and (b != b or b in (float('inf'), float('-inf'))):
+        raise TypeError("b must be a valid number")
 
-    a = int(a)
-    b = int(b)
-
-    return a + b
+    return int(a) + int(b)
