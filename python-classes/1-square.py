77caff34f14cf2ee3be5c676188aff56723e0a46
Etiennepev <etienneloichot@gmail.com>
2025-01-27 10:32:49 +0100

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module containing the definition of the Square class.
+
+This module provides a placeholder for a Square class that can be expanded
+to represent a square and its related properties or behaviors.
+"""
+
+
+class Square:
+    """
+    Representation of a square.
+    """
+    def __init__(self, size):
+        """
+        Initializes a square with a private size attribute.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
