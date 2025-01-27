f9d054f084a1bb072adfb2bf3b4dfcde980b1eed
Etiennepev <etienneloichot@gmail.com>
2025-01-27 10:19:27 +0100

    documentation style task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+"""
+Module containing the definition of the Square class.
+
+This module provides a placeholder for a Square class that can be expanded
+to represent a square and its related properties or behaviors.
+"""
+
 
 class Square:
+    """
+    Representation of a square.
+
+    This class is currently empty
+    """
     pass
