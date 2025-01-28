d4da243d5fda7fabedc7f3414c94c1be8d2d9a00
Etiennepev <etienneloichot@gmail.com>
2025-01-28 10:39:57 +0100

    task 0

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+"""
+Module: rectangle
+
+This module defines a basic Rectangle class. The class is currently empty
+and serves as a starting point for defining more complex behavior related
+to rectangles in future implementations.
+"""
+
 
 class Rectangle:
+    """
+    Representation of a rectangle.
+
+    This class is currently empty
+    """
     pass
