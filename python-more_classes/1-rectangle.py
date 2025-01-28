bb99bd0cc27f4e9ee8cdcb4f9512d9a7e3fd09b7
Etiennepev <etienneloichot@gmail.com>
2025-01-28 11:01:20 +0100

    task 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -21,8 +21,8 @@ class Rectangle:
             TypeError: If width or height are not integers.
             ValueError: If width or height are less than 0.
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
