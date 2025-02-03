a89a83f97b4717ad25166744b8682e1f2f2d1058
Etiennepev <etienneloichot@gmail.com>
2025-02-03 16:12:51 +0100

    task 7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -10,9 +10,15 @@ class BaseGeometry:
     Empty class
     """
     def area(self):
+        """
+        Raises an Exception
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """
+        Integer validator
+        """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
