7e248fba1da1df4a7aadc02cb7ffd1e74df1ff78
Etiennepev <etienneloichot@gmail.com>
2025-02-03 14:02:54 +0100

    is kind of class

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module: is same classe
+This module defines if is the same class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Prints the list in ascending order.
+
+    This method sorts the list and prints the sorted version, leaving the
+    original list unchanged.
+    """
+    return type(obj) is a_class
