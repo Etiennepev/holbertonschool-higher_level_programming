b0770370fe94368b7ebe32709315df1068d75621
Etiennepev <etienneloichot@gmail.com>
2025-02-04 14:36:18 +0100

    square 11 10

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -14,9 +14,14 @@ class Square(Rectangle):
         size (int): The size of the Square. Default is 0.
     """
     def __init__(self, size):
-        self.integer_validator("size", size)
-        super().__init__(size, size)
+        super().integer_validator(size, size)
         self.__size = size
 
     def area(self):
         return self.__size**2
+
+    def __str__(self):
+        """
+        Returns a string representation of the square.
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
