597cd30d58baa9faef55e6eef4a570a062390781
Etiennepev <etienneloichot@gmail.com>
2025-02-06 10:02:14 +0100

    task1

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,52 +1,95 @@
 #!/usr/bin/python3
+
 """
-Module: duck typing
-Provides a duck tyîng
+Module task_01_abc.py
+Provides an ABC class Shape
 """
+
 from abc import ABC, abstractmethod
-from math import pi
+import math
 
 
 class Shape(ABC):
     """
-    Abstact class of the Shape.
+    ABC class Shape
     """
+
     @abstractmethod
     def area(self):
+        """
+        area ABC method
+        """
         pass
 
     @abstractmethod
     def perimeter(self):
+        """
+        perimeter ABC method
+        """
         pass
-    
+
+
 class Circle(Shape):
-    def __init__(self, radius=0):
-        if isinstance is not (radius, (int, float)):
-            raise ValueError("Must be integer or float")
-        else:
-            self.radius = abs(radius)
+    """
+    Circle class
+    """
+
+    def __init__(self, radius):
+        """
+        Initialize the Circle class
+        """
+        self.__radius = abs(radius)
 
     def area(self):
-        return pi * (self.radius ** 2)
-    
+        """
+        area method
+        """
+        return math.pi * (self.__radius ** 2)
+
     def perimeter(self):
-        return pi * self.radius * 2
+        """
+        perimeter method
+        """
+        return math.pi * self.__radius * 2
+
 
 class Rectangle(Shape):
-    def __init__(self, width=0, height=0):
+    """
+    Rectangle class
+    """
+
+    def __init__(self, width, height):
+        """
+        Initialize the Rectangle class
+        """
         self.__width = width
         self.__height = height
 
     def area(self):
+        """
+        area method
+        """
         return self.__width * self.__height
 
     def perimeter(self):
-        return 2 * (self.__width + self.__height)
-    
+        """
+        perimeter method
+        """
+        return 2 * (self.__height + self.__width)
+
+
 def shape_info(shape):
     """
     shape_info methode
     """
-    if hasattr(shape,"area") and hasattr(shape, "perimeter"):
-        print("Area : {}".format(shape.area()))
-        print("Perimeter: {}".format(shape.perimeter()))
+    print(f"Area: {shape.area()}")
+    print(f"Perimeter: {shape.perimeter()}")
+
+
+if __name__ == "__main__":
+
+    circle = Circle(radius=5)
+    rectangle = Rectangle(width=4, height=7)
+
+    shape_info(circle)
+    shape_info(rectangle)
