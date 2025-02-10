94b8a881bfda5edff1e1d18f0243711c7b4efaf5
Etiennepev <etienneloichot@gmail.com>
2025-02-10 10:59:22 +0100

    json string

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -4,7 +4,11 @@ Module: json file
 Provides 'json file'
 """
 import json
+""" import json"""
 
 
 def to_json_string(my_obj):
+    """
+    use json for the representation of an object (string)
+    """
     return json.dumps(my_obj)
