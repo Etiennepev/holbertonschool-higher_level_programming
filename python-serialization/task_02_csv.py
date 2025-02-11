61af67e2240fd5e9a6f5811eaffc883c69deba37
Etiennepev <etienneloichot@gmail.com>
2025-02-11 13:46:10 +0100

    changing data.json and deleting the newline

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -10,10 +10,10 @@ import json
 
 def convert_csv_to_json(filename):
     try:
-        with open(filename, mode='r', newline='', encoding='utf-8') as csv_file:
+        with open(filename, mode='r', encoding='utf-8') as csv_file:
             csv_reader = csv.DictReader(csv_file)
             rows = list(csv_reader)
-        with open(filename, mode='w', encoding='utf-8') as json_file:
+        with open('data.json', mode='w', encoding='utf-8') as json_file:
             json.dump(rows, json_file, indent=4)
         return True
     except FileNotFoundError:
