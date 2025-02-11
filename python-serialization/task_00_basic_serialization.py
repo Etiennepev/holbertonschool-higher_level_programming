50a9af4f4f33eef9766853716f72668a683b48d0
Etiennepev <etienneloichot@gmail.com>
2025-02-11 09:59:16 +0100

    readme + basic serialization 0

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module: basic serialization
+Provides 'basic serialization'
+"""
+import pickle
+""" import pickle"""
+
+def serialize_and_save_to_file(data, filename):
+    with open(filename, "wb") as file:
+        pickle.dump(data, file)
+    pass
+
+def load_and_deserialize(filename):
+    with open(filename, "rb") as file:
+        pickle.load(file)
+    pass
