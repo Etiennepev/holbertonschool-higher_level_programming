388bab24d910683f1570f8541900e2ee72e40a36
Etiennepev <etienneloichot@gmail.com>
2025-02-11 10:45:47 +0100

    basic serialization

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -3,17 +3,17 @@
 Module: basic serialization
 Provides 'basic serialization'
 """
-import pickle
-""" import pickle"""
+import json
+""" import json"""
 
 
 def serialize_and_save_to_file(data, filename):
     with open(filename, "w") as file:
-        return pickle.dump(data, file)
+        return json.dump(data, file)
     pass
 
 
 def load_and_deserialize(filename):
     with open(filename, "r") as file:
-        return pickle.load(file)
+        return json.load(file)
     pass
