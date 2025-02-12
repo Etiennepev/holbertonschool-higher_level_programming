20810f109a19427c66c64b17facaeab455400e96
Etiennepev <etienneloichot@gmail.com>
2025-02-12 10:45:29 +0100

    pascal triangle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -6,11 +6,14 @@ Provides 'pascla triangle'
 
 
 def pascal_triangle(n):
-    triangle = [[1]]
-    for i in range(1, n):
+    if n <= 0:
+        return []
+    triangle = []
+    for i in range(n):
         row = [1]
         for j in range(1, i):
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)
+        if i > 0:
+            row.append(1)
         triangle.append(row)
     return triangle
