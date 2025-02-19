69e531c1d39d24f5cfaf10fb73e5d89d9dba1c5f
Etiennepev <etienneloichot@gmail.com>
2025-02-19 09:24:16 +0100

    task 4 flask modifying the info on jane and john

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -6,18 +6,7 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-users = {
-    "jane": {
-        "username": "jane",
-        "name": "Jane",
-        "age": 28,
-        "city": "Los Angeles"},
-    "john": {
-        "username": "john",
-        "name": "John",
-        "age": 30,
-        "city": "New York"}
-}
+users = {}
 
 
 @app.route("/")
@@ -59,4 +48,4 @@ def add_user():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
