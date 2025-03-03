06562e161b1ea71bc480c7d24cbb994d860d608e
Etiennepev <etienneloichot@gmail.com>
2025-03-03 12:31:14 +0100

    adding ';'

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
 -- Create database, if it's already exist, no error
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
 -- Delete a database if already delete no error
-DROP DATABASE IF EXISTS hbtn_0c_0
+DROP DATABASE IF EXISTS hbtn_0c_0;
