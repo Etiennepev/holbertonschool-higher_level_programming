af89d152629a2abd321bff4fdc561c04b4fe18b6
Etiennepev <etienneloichot@gmail.com>
2025-03-03 12:30:07 +0100

    Task 2 remove database + 0, 1 task for docstring

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,1 +1,2 @@
+-- Create database, if it's already exist, no error
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0
---
+++ SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- Delete a database if already delete no error
+DROP DATABASE IF EXISTS hbtn_0c_0
