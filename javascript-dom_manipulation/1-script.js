3f1af10319dbe862d84f12d8d650068ea594f098
Etiennepev <etienneloichot@gmail.com>
2025-04-03 10:51:42 +0200

    Task 0, 1, 2 + all empty files

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let redHeader = document.getElementById("red_header");
+    let header = document.querySelector("header");
+    redHeader.addEventListener("click", function () {
+        header.style.color = "#FF0000";
+    });
+});
---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let redHeader = document.getElementById("red_header");
+    let header = document.querySelector("header");
+    redHeader.addEventListener("click", function () {
+        header.classList.add("red");
+    });
+});
